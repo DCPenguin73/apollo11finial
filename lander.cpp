83e1ae4d405195a4b73fbbf6099474633d2ab636
CaydenLords <lor20002@byui.edu>
2023-10-06 14:03:43 -0600

    STUB FUNCTIONS STUB CLASSES I SURE LOVE STUBS

--- lander.cpp
+++ lander.cpp
@@ -2,4 +2,45 @@
 #include <iostream>  
 #include <cassert>   
 #include "lander.h"
+#include "point.h"   
+#include "angle.h"
+using std::string;
+using std::min;
+using std::max;
 using namespace std;
+
+
+double Lander::computeDistance(double speed, double velocity, double accel, double time) const {
+	//STUB FUNCTION SO COOL YEAH
+	return 0.0;
+};
+double Lander::calculateAcc(double force, double mass) const {
+	//STUB FUNCTION SO COOL YEAH
+	return 0.0;
+};
+double Lander::calculateVel(double velocity, double acceleration, double time) const {
+	//STUB FUNCTION SO COOL YEAH
+	return 0.0;
+};
+double Lander::calculateHor(Angle angle, double total) const {
+	//STUB FUNCTION SO COOL YEAH
+	return 0.0;
+};
+double Lander::calculateVer(Angle angle, double total) const {
+	//STUB FUNCTION SO COOL YEAH
+	return 0.0;
+};
+double Lander::calculateTotal(double x, double y) const {
+	//STUB FUNCTION SO COOL YEAH
+	return 0.0;
+};
+void Lander::moveLander() {
+	//STUB FUNCTION SO COOL YEAH
+};
+void Lander::setFuel(int change) {
+	//STUB FUNCTION SO COOL YEAH
+};
+int Lander::getFuel() const {
+	//STUB FUNCTION SO COOL YEAH
+	return 0;
+};
--- lander.h
+++ lander.h
@@ -9,22 +9,60 @@ using std::string;
 using std::min;
 using std::max; using namespace std;
 class Lander {
+public:
+	Lander() {
+		location = Point(0, 0);
+		xAcc = 0;
+		yAcc = 0;
+		xVel = 0;
+		yVel = 0;
+		landerAngle = Angle(0);
+		speed = 0;
+		fuel = 5000;
+	}
+	Lander(const Lander& copier) {
+		location = copier.location;
+		xAcc = copier.xAcc;
+		yAcc = copier.yAcc;
+		xVel = copier.xVel;
+		yVel = copier.yVel;
+		landerAngle = copier.landerAngle;
+		speed = copier.speed;
+		fuel = copier.fuel;
+		bool outOfFuel = copier.outOfFuel;
+	}
+	Lander(Point inLocation, double inXAcc, double inYAcc, double inXVel, double inYVel, Angle inLanderAngle, double inSpeed) {
+		location = inLocation;
+		xAcc = inXAcc;
+		yAcc = inYAcc;
+		xVel = inXVel;
+		yVel = inYVel;
+		landerAngle = inLanderAngle;
+		speed = inSpeed;
+		fuel = 5000;
+		outOfFuel = false;
+	}
 private:
 	Point location;
 	double xAcc;
 	double yAcc;
 	double xVel;
 	double yVel;
-	Angle angle;
+	Angle landerAngle;
 	double speed;
 	int fuel;
-	const double GRAVITY;
-	const double THRUST;
+	const double GRAVITY = 10;
+	double THRUSTSTRENGTH = 10;
 	bool outOfFuel;
-public:
+private:
 	double computeDistance(double speed, double velocity, double accel, double time) const;
-	double calculateAcc();
-	double calculateVel();
-	double calculateHor();
-	double calculateVer();
+	double calculateAcc(double force, double mass) const;
+	double calculateVel(double velocity, double acceleration, double time) const;
+	double calculateHor(Angle angle, double total) const;
+	double calculateVer(Angle angle, double total) const;
+	double calculateTotal(double x, double y) const;
+public:
+	void moveLander();
+	void setFuel(int change);
+	int getFuel() const;
 };
