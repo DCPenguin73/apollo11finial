09ee5cbc5613decae24c33b6a5de893edda390ed
CaydenLords <lor20002@byui.edu>
2023-10-20 13:22:01 -0600

    ADDING COMMENTS YAAAAY

--- lander.cpp
+++ lander.cpp
@@ -1,3 +1,13 @@
+/***********************************************************************
+ * Source File:
+ *    Lander: represents the lander we control in the moon lander simulator. 
+ * Author:
+ *    Cayden Lords and Daniel Carr
+ * Summary:
+ *    Where the lander is and how it flies. 
+ ************************************************************************/
+
+
 #include <math.h>   
 #include <cmath>
 #include <iostream>  
@@ -12,29 +22,59 @@ using std::min;
 using std::max;
 using namespace std;
 
-
+/************************************************************************
+ * COMPUTE DISTANCE
+ * Calculate how far the lander would move
+ ************************************************************************/
 double Lander::computeDistance(double location, double velocity, double accel, double time) const {
 	double newLocation = location + (velocity * time) + (0.5 * accel * (time * time));
 	return newLocation;
 };
+
+/************************************************************************
+ * CALCULATE VELOCITY
+ * Calculate the new velocity of the lander
+ ************************************************************************/
 double Lander::calculateVel(double velocity, double acceleration, double time) const {
 	double newVelocity = velocity + (acceleration * time);
 	return newVelocity;
 };
+
+/************************************************************************
+ * CALCULATE HORIZONTAL
+ * Calculate an x variable based on the total and the angle
+ ************************************************************************/
 double Lander::calculateHor(Angle angle, double total) const {
 	double x = sin(angle.getRadians()) * total;
 	return x;
 };
+
+/************************************************************************
+ * CALCULATE HORIZONTAL
+ * Calculate an y variable based on the total and the angle
+ ************************************************************************/
 double Lander::calculateVer(Angle angle, double total) const {
 	double y = cos(angle.getRadians()) * total;
 	return y;
 };
+
+/************************************************************************
+ * CALCULATE TOTAL
+ * Calculate calculate the total based on the x and y ranges
+ ************************************************************************/
 double Lander::calculateTotal(double x, double y) const {
 	double total;
 	total = sqrt(x * x + y * y);
 	return total;
 };
+
+/************************************************************************
+ * MOVE LANDER
+ * Updates all the variables of the lander and moves it to its new location
+ ************************************************************************/
 void Lander::moveLander(bool upPressed, bool leftPressed, bool rightPressed) {
+	
+	// If the fuel isn't out, recieve the user's controls. 
 	if (outOfFuel == false) {
 		if (rightPressed) {
 			landerAngle.setRadians(landerAngle.getRadians() - .1);
@@ -52,6 +92,8 @@ void Lander::moveLander(bool upPressed, bool leftPressed, bool rightPressed) {
 			thrusterOn = false;
 		}
 	}
+
+	// Update the acceleration based on whether the thruster is on 
 	if (thrusterOn == true) {
 		xAcc = calculateHor(landerAngle.getRadians()+PI, THRUSTSTRENGTH);
 		yAcc = calculateVer(landerAngle.getRadians(), THRUSTSTRENGTH) + GRAVITY;
@@ -60,23 +102,49 @@ void Lander::moveLander(bool upPressed, bool leftPressed, bool rightPressed) {
 		xAcc = 0;
 		yAcc = GRAVITY;
 	}
+
+	// Update the horizontal and verical speed
 	xVel = calculateVel(xVel, xAcc, time);
 	yVel = calculateVel(yVel, yAcc, time);
+
+	// Use the new speeds to update the overall speed
 	speed = calculateTotal(xVel, yVel);
+
+	// Move the lander based on the speed it is moving
 	location = Point(
 		computeDistance(location.getX(), xVel, xAcc, time),
 		computeDistance(location.getY(), yVel, yAcc, time)
 	);
 };
+
+/************************************************************************
+ * GET FUEL
+ * Return the current amount of fuel
+ ************************************************************************/
 int Lander::getFuel() const {
 	return fuel;
 };
+
+/************************************************************************
+ * GET LOCATION
+ * Return the current location
+ ************************************************************************/
 Point Lander::getLocation() const {
 	return location;
 }
+
+/************************************************************************
+ * GET LANDER
+ * Return the current angle of the lander
+ ************************************************************************/
 Angle Lander::getAngle() const {
 	return landerAngle;
 }
+
+/************************************************************************
+ * LOSE FUEL
+ * Reduce the amount of fuel the lander has
+ ************************************************************************/
 void Lander::loseFuel(int change){
 	fuel = fuel - change;
 	if (fuel <= 0) {
@@ -84,6 +152,11 @@ void Lander::loseFuel(int change){
 		outOfFuel = true;
 	}
 }
+
+/************************************************************************
+ * GET SPEED
+ * Return the current overall speed of the lander
+ ************************************************************************/
 double Lander::getSpeed() const {
 	return speed;
 }
--- lander.h
+++ lander.h
@@ -1,3 +1,11 @@
+/***********************************************************************
+*    Lander: represents the lander we control in the moon lander simulator.
+ * Author:
+ *    Cayden Lords and Daniel Carr
+ * Summary:
+ *    Where the lander is and how it flies.
+ ************************************************************************/
+
 #pragma once
 #include <string>     // To display text on the screen
 #include <cmath>      // for M_PI, sin() and cos()
@@ -5,13 +13,18 @@
 #include <sstream>    // for OSTRINGSTRING
 #include "point.h"    // Where things are drawn
 #include "angle.h"
-#include "uiInteract.h"
 #define PI 3.14159265359
 using std::string;
 using std::min;
 using std::max; using namespace std;
+
+/***********************************************************
+ * LANDER
+ * The lander class
+ ***********************************************************/
 class Lander {
 public:
+	//default constructor
 	Lander() {
 		location = Point(300, 300);
 		xAcc = 0;
@@ -23,6 +36,7 @@ public:
 		fuel = 5000;
 		time = .1;
 	}
+	//copier constructor
 	Lander(const Lander& copier) {
 		location = copier.location;
 		xAcc = copier.xAcc;
@@ -35,6 +49,7 @@ public:
 		outOfFuel = copier.outOfFuel;
 		time = copier.time;
 	}
+	//Parameterized constructor
 	Lander(Point inLocation, double inXAcc, double inYAcc, double inXVel, double inYVel, Angle inLanderAngle, double inSpeed) {
 		location = inLocation;
 		xAcc = inXAcc;
@@ -48,32 +63,53 @@ public:
 		time = .1;
 	}
 public: 
-	bool outOfFuel;
+	bool outOfFuel;                                // Whether the lander is out of fuel or not
 private:
-	Point location;
-	double xAcc;
-	double yAcc;
-	double xVel;
-	double yVel;
-	Angle landerAngle;
-	double speed;
-	int fuel;
-	const double GRAVITY = -1.625;
-	const double THRUSTSTRENGTH = 45000/15103 *3;
-	double thrusterOn = false;
-	double time;
+	Point location;                                // Where the fuel is 
+	double xAcc;                                   // The horizontal acceleration of the lander
+	double yAcc;                                   // The vertical acceleration of the lander
+	double xVel;                                   // The horizontal velocity of the lander
+	double yVel;                                   // The vertical velocity of the lander
+	Angle landerAngle;                             // What angle the lander is facing at
+	double speed;                                  // The overall velocity or speed of the lander
+	int fuel;                                      // How much fuel the lander has left
+	const double GRAVITY = -1.625;                 // The amount of gravity pulling on the lander
+	const double THRUSTSTRENGTH = 45000/15103 *3;  // How strong the thrust on the lander is
+	double thrusterOn = false;                     // Whether the thruster is on or not
+	double time;                                   // How long between calculations of the lander moving
 private:
+	// calculate the distance moved based on velocity and acceleration
 	double computeDistance(double location, double velocity, double accel, double time) const;
+	
+	// calculates the new velocity after a segment of time
 	double calculateVel(double velocity, double acceleration, double time) const;
+	
+	// calculates the horizontal movement from the total movement and the angle
 	double calculateHor(Angle angle, double total) const;
+	
+	// calculates the vertical movement from the total movement and the angle
 	double calculateVer(Angle angle, double total) const;
+
+	// calculate the total movment based on the x and y movement.
 	double calculateTotal(double x, double y) const;
+
 public:
+	// calculates where the lander should move and how fast it's moving. 
 	void moveLander(bool upPressed, bool leftPressed, bool rightPressed);
+	
+	// returns how much fuel is left
 	int getFuel() const;
+
+	// returns where the lander currently is
 	Point getLocation() const;
+
+	// returns what direction the lander is facing
 	Angle getAngle() const;
+
+	// removes fuel as the player uses it 
 	void loseFuel(int change);
+
+	//returns how fast the speeder is moving overall
 	double getSpeed() const;
 
 };
